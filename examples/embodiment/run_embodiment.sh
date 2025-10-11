6508264d7a7e02d53fe50d45ce72b2fa5320114c
Hao Lin <linhaomails@gmail.com>
2025-10-11 02:58:51 +0000

    fix: move LIBERO to RLinf version
    Signed-off-by: Hao Lin <linhaomails@gmail.com>

--- examples/embodiment/run_embodiment.sh
+++ examples/embodiment/run_embodiment.sh
@@ -7,12 +7,8 @@ export SRC_FILE="${EMBODIED_PATH}/train_embodied_agent.py"
 export MUJOCO_GL="egl"
 export PYOPENGL_PLATFORM="egl"
 export PYTHONPATH=${REPO_PATH}:$PYTHONPATH
-# NOTE: set LIBERO_REPO_PATH to the path of the LIBERO repo
-export LIBERO_REPO_PATH="/path/to/repo/LIBERO"
-# NOTE: set LIBERO_CONFIG_PATH for libero/libero/__init__.py
-export LIBERO_CONFIG_PATH=${LIBERO_REPO_PATH}
+export LIBERO_CONFIG_PATH="/path/to/repo/LIBERO_CONFIG_PATH"
 
-export PYTHONPATH=${LIBERO_REPO_PATH}:$PYTHONPATH
 export CUDA_LAUNCH_BLOCKING=1
 export HYDRA_FULL_ERROR=1
 
