13c9bd3f1c782dcbd2c41a59e43d17471bee12b7
Yun Liu <yunl@nvidia.com>
2026-01-27 15:34:00 +0800

    fix format
    Signed-off-by: Yun Liu <yunl@nvidia.com>

--- rlinf/scheduler/worker/worker.py
+++ rlinf/scheduler/worker/worker.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import importlib
 import ctypes
 import functools
+import importlib
 import inspect
 import logging
 import os
