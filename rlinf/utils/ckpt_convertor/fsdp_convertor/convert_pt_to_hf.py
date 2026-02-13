87590d8d7b9732d994470417f7d6cf4f1270aa15
Hao Lin <linhaomails@gmail.com>
2026-02-13 04:59:42 +0000

    fix: make linter happy
    Signed-off-by: Hao Lin <linhaomails@gmail.com>

--- rlinf/utils/ckpt_convertor/fsdp_convertor/convert_pt_to_hf.py
+++ rlinf/utils/ckpt_convertor/fsdp_convertor/convert_pt_to_hf.py
@@ -18,7 +18,7 @@ import hydra
 import torch
 
 from rlinf.models import get_model
-from toolkits.ckpt_convertor.fsdp_convertor.utils import (
+from rlinf.utils.ckpt_convertor.fsdp_convertor.utils import (
     copy_model_config_and_code,
     get_model_save_helper,
     save_state_dict_sharded_safetensors,
