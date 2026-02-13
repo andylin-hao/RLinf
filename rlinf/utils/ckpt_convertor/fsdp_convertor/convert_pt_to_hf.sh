c6493522d7a566fe550e62949d16887a0f971b73
Hao Lin <linhaomails@gmail.com>
2026-02-13 08:18:11 +0000

    fix: ckpt convertor path
    Signed-off-by: Hao Lin <linhaomails@gmail.com>

--- rlinf/utils/ckpt_convertor/fsdp_convertor/convert_pt_to_hf.sh
+++ rlinf/utils/ckpt_convertor/fsdp_convertor/convert_pt_to_hf.sh
@@ -4,8 +4,8 @@ set -x
 tabs 4
 
 export FSDP_CONVERTOR_PATH="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export REPO_PATH=$(dirname $(dirname $(dirname "$FSDP_CONVERTOR_PATH")))
+export REPO_PATH=$(dirname $(dirname $(dirname $(dirname "$FSDP_CONVERTOR_PATH"))))
 export PYTHONPATH=${REPO_PATH}:$PYTHONPATH
 
 
-python ${REPO_PATH}/toolkits/ckpt_convertor/fsdp_convertor/convert_pt_to_hf.py --config-path ${REPO_PATH}/toolkits/ckpt_convertor/fsdp_convertor/config --config-name fsdp_model_convertor
+python ${REPO_PATH}/rlinf/utils/ckpt_convertor/fsdp_convertor/convert_pt_to_hf.py --config-path ${REPO_PATH}/rlinf/utils/ckpt_convertor/fsdp_convertor/config --config-name fsdp_model_convertor
